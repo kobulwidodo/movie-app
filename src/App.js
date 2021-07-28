016d402abff38dbb94831c491527852fa41a0e0f
kobulwidodo <fadhilhan01@gmail.com>
2021-07-29 02:09:27 +0700

    Add route and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GlobalStyle from './styles/globalStyles';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <GlobalStyle />
+        <Switch>
+            <Route path="/" exact component={HomePage} />
+        </Switch>
+    </Router>
   );
 }
 
