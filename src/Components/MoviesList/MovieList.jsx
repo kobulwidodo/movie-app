85302317883ccab267638bfd3b19021b569af82a
kobulwidodo <fadhilhan01@gmail.com>
2021-07-29 18:59:43 +0700

    Change some logic and add skeleton

--- src/Components/MoviesList/MovieList.jsx
+++ src/Components/MoviesList/MovieList.jsx
@@ -1,20 +1,22 @@
 import { Col, Row } from 'react-bootstrap'
 import CardMovies from '../CardMovies/CardMovies'
+import Skeleton from '@material-ui/lab/Skeleton'
+import styled from 'styled-components'
+
+const SkeletonMovie = styled(Skeleton)`
+    border-radius: 15px;
+    margin: 22px;
+`
 
 const MovieList = (props) => {
     return (
         <>
             <Row>
-                { props.isLimit4 ? props.moviesList.slice(0, 4).map((movie) => (
-                    <Col lg={3} sm={6} key={movie.id}>
-                        <CardMovies
-                            key={movie.id}
-                            ImagePoster={movie.poster_path}
-                            TitlePoster={movie.title ?? movie.name}
-                            DatePoster={movie.release_date ?? movie.first_air_date}
-                        />
+                {props.loading ? [...Array(props.isLimit4 ? 4 : 20)].map((_, index) => (
+                    <Col lg={3} sm={6} key={index}>
+                        <SkeletonMovie animation="wave" variant="rect" widht="100%" height={300}/>
                     </Col>
-                )) : props.moviesList.map((movie) => (
+                )) : props.moviesList.slice(0, props.isLimit4 ? 4 : 20).map((movie) => (
                     <Col lg={3} sm={6} key={movie.id}>
                         <CardMovies
                             key={movie.id}
