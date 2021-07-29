a1121f185af18983de980de279182a85ab510ba0
kobulwidodo <fadhilhan01@gmail.com>
2021-07-29 17:35:23 +0700

    Remove fetching data

--- src/Components/MoviesList/MovieList.jsx
+++ src/Components/MoviesList/MovieList.jsx
@@ -1,47 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import movieapi from '../../api/movieapi'
+import { Col, Row } from 'react-bootstrap'
 import CardMovies from '../CardMovies/CardMovies'
 
 const MovieList = (props) => {
-    const [moviesList, setMoviesList] = useState([])
-    const moviesArray = JSON.stringify(moviesList)
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await movieapi.get(`/discover/movie`, {
-                params: { },
-            })
-            setMoviesList(res.data.results)
-        }
-        fetchData()
-    }, [moviesArray])
-
     return (
         <>
-            <Container>
-                <Row>
-                    { props.isLimit4 ? moviesList.slice(0, 4).map((movie) => (
-                        <Col lg={3} sm={6} key={movie.id}>
-                            <CardMovies
-                                key={movie.id}
-                                ImagePoster={movie.poster_path}
-                                TitlePoster={movie.title}
-                                DatePoster={movie.release_date}
-                            />
-                        </Col>
-                    )) : moviesList.map((movie) => (
-                        <Col lg={3} sm={6} key={movie.id}>
-                            <CardMovies
-                                key={movie.id}
-                                ImagePoster={movie.poster_path}
-                                TitlePoster={movie.title}
-                                DatePoster={movie.release_date}
-                            />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+            <Row>
+                { props.isLimit4 ? props.moviesList.slice(0, 4).map((movie) => (
+                    <Col lg={3} sm={6} key={movie.id}>
+                        <CardMovies
+                            key={movie.id}
+                            ImagePoster={movie.poster_path}
+                            TitlePoster={movie.title ?? movie.name}
+                            DatePoster={movie.release_date ?? movie.first_air_date}
+                        />
+                    </Col>
+                )) : props.moviesList.map((movie) => (
+                    <Col lg={3} sm={6} key={movie.id}>
+                        <CardMovies
+                            key={movie.id}
+                            ImagePoster={movie.poster_path}
+                            TitlePoster={movie.title ?? movie.name}
+                            DatePoster={movie.release_date ?? movie.first_air_date}
+                        />
+                    </Col>
+                ))}
+            </Row>
         </>
     )
 }
