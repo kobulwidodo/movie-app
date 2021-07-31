36cd15b8f1fd0ebaa28cc6b4fc953c2c9dcf80c6
kobulwidodo <fadhilhan01@gmail.com>
2021-07-31 16:48:36 +0700

    Add scroll to top and tv shows route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './styles/globalStyles';
 import HomePage from './Pages/HomePage';
 import MoviesPage from './Pages/MoviesPage';
+import TvShowsPage from './Pages/TvShowsPage';
+import ScrollToTop from './Components/Utilites/ScrollToTop';
 
 function App() {
   return (
     <Router>
         <GlobalStyle />
+        <ScrollToTop />
         <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/movies" exact component={MoviesPage} />
+            <Route path="/tv-shows" exact component={TvShowsPage} />
         </Switch>
     </Router>
   );
